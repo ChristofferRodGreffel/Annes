238333fbfe80066a2305d8cbe18b1ea046839280
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-11-30 11:31:09 +0100

    more work

--- src/pages/admin/CreateMenuProduct.jsx
+++ src/pages/admin/CreateMenuProduct.jsx
@@ -1,60 +1,53 @@
-import React, { useRef, useState } from 'react'
-import PageWrapperContainer from '../../components/PageWrapperContainer'
-import PageH1Title from '../../components/PageH1Title'
-import CustomInputWithLabel from '../../components/CustomInputWithLabel'
+import React, { useRef, useState } from "react";
+import PageWrapperContainer from "../../components/PageWrapperContainer";
+import PageH1Title from "../../components/PageH1Title";
+import CustomInputWithLabel from "../../components/CustomInputWithLabel";
 
 function CreateMenuProduct() {
+  const formRef = useRef(null);
 
-    const formRef = useRef(null)
+  // skal evt. komme fra Firebase
+  const breadTypes = [{ name: "Lyst" }, { name: "Mørkt" }];
 
-    // skal evt. komme fra Firebase
-    const breadTypes = [
-        { name: "Lyst" },
-        { name: "Mørkt" },
-    ];
+  const [checkedBreadTypes, setCheckedBreadTypes] = useState(new Array(breadTypes.length).fill(false));
 
-    const [checkedBreadTypes, setCheckedBreadTypes] = useState(
-        new Array(breadTypes.length).fill(false)
-    );
+  const handleOnBreadtypeChange = (position) => {
+    const updatedCheckedState = checkedBreadTypes.map((item, index) => (index === position ? !item : item));
+    setCheckedBreadTypes(updatedCheckedState);
+  };
 
-    const handleOnBreadtypeChange = (position) => {
-        const updatedCheckedState = checkedBreadTypes.map((item, index) =>
-            index === position ? !item : item
-        );
-        setCheckedBreadTypes(updatedCheckedState);
-    };
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    let productName = formRef.current.productName.value;
 
-    const handleAddProduct = (e) => {
-        e.preventDefault()
-        let productName = formRef.current.productName.value
+    let checkedBreadOptions = [];
 
-        let checkedBreadOptions = []
+    checkedBreadTypes.forEach((checked, index) => {
+      if (checked === true) {
+        checkedBreadOptions.push(breadTypes[index].name);
+      }
+    });
 
-        checkedBreadTypes.forEach((checked, index) => {
-            if (checked === true) {
-                checkedBreadOptions.push(breadTypes[index].name)
-            }
-        })
+    console.log(checkedBreadOptions);
+  };
 
-        console.log(checkedBreadOptions)
-    }
+  return (
+    <>
+      <PageH1Title>Tilføj nyt produkt</PageH1Title>
 
-    return (
-        <PageWrapperContainer>
-            <PageH1Title>
-                Tilføj nyt produkt
-            </PageH1Title>
+      <form ref={formRef} onSubmit={handleAddProduct}>
+        <CustomInputWithLabel label="Navn på product" type="text" name="productName" placeholder="Skriv produkt navn" />
+        <CustomInputWithLabel
+          type="checkbox"
+          CustomHandleChange={handleOnBreadtypeChange}
+          CustomCheckedItems={checkedBreadTypes}
+          CustomOptions={breadTypes}
+        />
 
-            <form ref={formRef} onSubmit={handleAddProduct}>
-                <CustomInputWithLabel label="Navn på product" type="text" name="productName" placeholder="Skriv produkt navn" />
-                <CustomInputWithLabel type="checkbox" CustomHandleChange={handleOnBreadtypeChange} CustomCheckedItems={checkedBreadTypes} CustomOptions={breadTypes} />
-
-                <button type='submit'>Tilføj</button>
-
-            </form>
-
-        </PageWrapperContainer>
-    )
+        <button type="submit">Tilføj</button>
+      </form>
+    </>
+  );
 }
 
-export default CreateMenuProduct
+export default CreateMenuProduct;
