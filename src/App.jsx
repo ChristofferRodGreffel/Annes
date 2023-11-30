23d369cf13c5d37eab9512a46d419ce24e0da526
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-11-30 09:52:42 +0100

    work

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ function App() {
   return (
     <>
       <PageWrapperContainer>
-          <div className="bg-primary">
-            <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-          </div>
-          <div className="bg-red-500 breakout md:bg-blue-200">
-            <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-          </div>
-          <div className="bg-red-500 full-width">
-            <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-          </div>
+        <div className="bg-primary">
+          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
+        </div>
+        <div className="bg-main-grey breakout">
+          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
+        </div>
+        <div className="bg-red full-width">
+          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
+        </div>
       </PageWrapperContainer>
     </>
   );
---
+++ src/components/AdminSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./assets/logo.png";
+
+const AdminSidebar = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default AdminSidebar;
