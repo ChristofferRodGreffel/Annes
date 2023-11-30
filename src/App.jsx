e131b543be944e248c381fdf7027ed8f4af11bb6
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-11-30 09:58:36 +0100

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import PageWrapperContainer from "./components/PageWrapperContainer";
 import CreateMenuProduct from "./pages/admin/CreateMenuProduct";
+import AdminSidebar from "./components/AdminSidebar";
+import LandingPage from "./pages/customer/LandingPage";
 
 function App() {
 
@@ -9,22 +11,10 @@ function App() {
   return (
     <>
       <Routes>
-
         <Route path="/admin-sidebar" element={<AdminSidebar />} />
         <Route path="/opret-produkt" element={<CreateMenuProduct />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
-      <PageWrapperContainer>
-        <div className="bg-primary">
-          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-        </div>
-        <div className="bg-violet-500 breakout md:bg-blue-200">
-          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-        </div>
-        <div className="bg-pink-500 full-width">
-          <p className="text-primaryTest text-2xl text-opacity-20">Test</p>
-        </div>
-      </PageWrapperContainer>
     </>
   );
 }
