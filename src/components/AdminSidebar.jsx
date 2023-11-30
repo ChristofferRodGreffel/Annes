e9974d6e4349ae4d833046b7f61cbd8fc7339340
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-11-30 10:03:02 +0100

    work in progress

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const AdminSidebar = () => {
   return (
     <div>
       <img src={logo} alt="logo" />
+      <nav>
+        <Link to={"#"}>
+          <i className="fa-solid fa-house"></i>
+        </Link>
+      </nav>
     </div>
   );
 };
