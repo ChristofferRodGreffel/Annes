7bbf6b7444ca0463dab269156d9c136846dffe1f
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-12-04 10:42:39 +0100

    customize page progress

--- src/pages/customer/CustomizeProduct.jsx
+++ src/pages/customer/CustomizeProduct.jsx
@@ -26,8 +26,10 @@ const CustomizeProduct = () => {
         <img className="full-width mt-5" src={productInfo?.imageURL} alt={`Billede af ${productInfo?.name}`} />
         <div className="mt-8">
           <h1 className="text-3xl font-bold">{productInfo?.name}</h1>
-          <div className="flex flex-col w-fit">
-            <label htmlFor="breadSelect">Vælg brød</label>
+          <div className="flex flex-col w-fit mt-5">
+            <label className="text-lg font-semibold mb-1" htmlFor="breadSelect">
+              Vælg brød
+            </label>
             <select
               className="border-2 border-dark rounded-full w-full py-1 px-3 font-medium"
               name="breadSelect"
@@ -42,8 +44,10 @@ const CustomizeProduct = () => {
               })}
             </select>
           </div>
-          <div>
-            <label htmlFor="ingredientsForm">Standard ingredienser</label>
+          <div className="mt-5">
+            <label className="text-lg font-semibold" htmlFor="ingredientsForm">
+              Standard ingredienser
+            </label>
             <form name="ingredientsForm" className="grid grid-cols-2 w-fit">
               {productInfo?.chosenIngredients?.map((ingredient, key) => {
                 return (
@@ -55,16 +59,41 @@ const CustomizeProduct = () => {
               })}
             </form>
           </div>
-          <div className="flex flex-row w-fit">
-            <label htmlFor="dressingSelect">Top</label>
-            <select
-              className="border-2 border-dark rounded-full w-full py-1 px-3 font-medium"
-              name="dressingSelect"
-              id="dressingSelect"
-            >
-              <option value="almindelig">Almindelig</option>
-              <option value="karry">Karry</option>
-            </select>
+          <div className="mt-5">
+            <h2 className="mb-1 text-lg font-semibold">Vælg dressing</h2>
+            <div className="flex flex-col gap-3 w-2/3">
+              <div className="flex flex-row w-full items-center justify-between gap-3">
+                <label htmlFor="dressingSelect">Top</label>
+                <select
+                  className="border-2 border-dark rounded-full py-1 px-3 font-medium w-fit"
+                  name="dressingSelect"
+                  id="dressingSelect"
+                  defaultValue="almindelig"
+                >
+                  <option value="almindelig">Almindelig</option>
+                  <option value="karry">Karry</option>
+                  <option value="pesto">Grøn pesto</option>
+                  <option value="chilimayo">Chilimayo +5 kr.</option>
+                </select>
+              </div>
+              <div className="flex flex-row w-full items-center justify-between gap-3">
+                <label htmlFor="dressingSelect">Bund</label>
+                <select
+                  className="border-2 border-dark rounded-full w-fit py-1 px-3 font-medium"
+                  name="dressingSelect"
+                  id="dressingSelect"
+                  defaultValue="almindelig"
+                >
+                  <option value="almindelig">Almindelig</option>
+                  <option value="karry">Karry</option>
+                  <option value="pesto">Grøn pesto</option>
+                  <option value="chilimayo">Chilimayo +5 kr.</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h2 className="text-lg font-semibold">Ekstra fyld</h2>
           </div>
         </div>
       </PageWrapperContainer>
