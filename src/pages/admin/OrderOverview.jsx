7ecad2f488bb552a9dfcc83804c724defc9660c2
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-12-07 10:49:48 +0100

    Mark new orders as recived

--- src/pages/admin/OrderOverview.jsx
+++ src/pages/admin/OrderOverview.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminContentWrapper from "../../components/AdminContentWrapper";
 import AdminSidebar from "../../components/AdminSidebar";
 
+import { collection, query, where, onSnapshot, updateDoc, doc } from "firebase/firestore";
+import { FIREBASE_DB } from "../../../firebase-config";
+
+
 const OrderOverview = () => {
+
+  useEffect(() => {
+
+    const MarkAsRecived = async (orderRef) => {
+      // To update age and favorite color:
+      await updateDoc(orderRef, {
+        status: "recieved",
+      });
+    }
+    const reciveAndMarkNewOrders = () => {
+      // Recive new orders and mark them as "modtaget" 
+      const q = query(collection(FIREBASE_DB, "orders"), where("status", "==", "pending"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach((orderDoc) => {
+          const orderRef = doc(FIREBASE_DB, "orders", orderDoc.id);
+          MarkAsRecived(orderRef)
+        })
+      })
+    }
+    reciveAndMarkNewOrders()
+  }, [])
+
   return (
     <div className="flex justify-center flex-row">
       <AdminSidebar />
-      <AdminContentWrapper></AdminContentWrapper>
+      <AdminContentWrapper>
+
+      </AdminContentWrapper>
     </div>
   );
 };
