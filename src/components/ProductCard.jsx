2cbf837ce318db601b1f4f4a5f967592301e334b
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-12-07 20:38:21 +0100

    blur on div

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 const ProductCard = (props) => {
 
   const [loadedImage, setLoadedImage] = useState(false)
-  
-  return (
-    <div className="rounded-xl overflow-clip text-white md:w-[350px]">
 
-      <img
-        loading="lazy"
-        className={`h-28 w-full object-cover object-center transition-all duration-1000 ${loadedImage ? 'opacity-100' : 'opacity-50 blur-lg'}`}
-        src={props.imageSource}
-        alt={`Billede af ${props.imageName}`}
-        onLoad={() => {setLoadedImage(true)}}
-      />
-      <div className="bg-primary productOverviewMinHeight">
-        <div className="flex flex-col justify-evenly h-auto min-h-[125px] w-full px-5 py-3 lg:w-[350px]">
-          <h3 className="font-bold text-xl mb-3 customBalance">{props.productName}</h3>
-          <button onClick={props.function} className="bg-white text-dark font-semibold p-1.5 w-full rounded-md text-lg">
-            {props.text} {props.icon && <i className={props.icon}></i>}
-          </button>
+  return (
+    <>
+      <div className={`rounded-xl overflow-clip text-white md:w-[350px] ${loadedImage ? null : 'blur-lg'}`}>
+          <img
+            loading="lazy"
+            className={` max-h-28 h-28 w-full object-cover object-center`}
+            src={props.imageSource}
+            alt={`Billede af ${props.imageName}`}
+            onLoad={() => { setLoadedImage(true) }}
+          />
+        <div className="bg-primary productOverviewMinHeight">
+          <div className="flex flex-col justify-evenly h-auto min-h-[125px] w-full px-5 py-3 lg:w-[350px]">
+            <h3 className="font-bold text-xl mb-3 customBalance">{props.productName}</h3>
+            <button onClick={props.function} className="bg-white text-dark font-semibold p-1.5 w-full rounded-md text-lg">
+              {props.text} {props.icon && <i className={props.icon}></i>}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
