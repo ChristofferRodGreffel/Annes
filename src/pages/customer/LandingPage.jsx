2b440d3c19686ca8a08d14da347faa0674d94fd0
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-12-10 20:05:43 +0100

    Typo

--- src/pages/customer/LandingPage.jsx
+++ src/pages/customer/LandingPage.jsx
@@ -63,7 +63,7 @@ function LandingPage() {
       <CustomerHeader nav={true} iconLeft="fa-solid fa-bars" iconRight="fa-solid fa-basket-shopping" />
       <PageWrapperContainer>
         <div className="mt-16 lg:w-1/3 lg:m-auto lg:mt-16">
-          <h2 className="text-center mb-3">Vælg mellem 5-6 brødtypper</h2>
+          <h2 className="text-center mb-3">Vælg mellem 6-7 brødtypper</h2>
           <div className="flex flex-col gap-2">
             <div className="flex justify-between border-b-2 border-grey border-dashed">
               <p>Sandwich & glutenfri</p>
