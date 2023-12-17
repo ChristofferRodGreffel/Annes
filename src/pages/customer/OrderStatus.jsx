216a5325d021283771ad63443f18e98109787d28
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-12-17 12:03:50 +0100

    Loading state på orderStatus

--- src/pages/customer/OrderStatus.jsx
+++ src/pages/customer/OrderStatus.jsx
@@ -11,6 +11,7 @@ import UpdatesBar from "../../components/UpdatesBar";
 import { timestampConvert } from "../../helperfunctions/TimestampConvert";
 import { toast } from "react-toastify";
 import { DefaultToastifySettings } from "../../helperfunctions/DefaultToastSettings";
+import { PulseLoader } from "react-spinners";
 
 // Udviklet fælles i gruppen
 
@@ -19,16 +20,23 @@ const OrderStatus = () => {
   const [currentOrderId, setCurrentOrderId] = useState("");
   const [currentOrder, setCurrentOrder] = useState();
 
+  const [loading, setLoading] = useState(true)
+
   // Kig først efter ordre id'et i params, ellers i localStorage
   useEffect(() => {
+    setLoading(true)
     if (!orderId) {
-      setCurrentOrderId(JSON.parse(localStorage.getItem("currentOrder")));
+      setCurrentOrderId(JSON.parse(localStorage.getItem("currentOrder")))
+      setLoading(false)
+      return
     }
 
     // Prøver først ID fra params, ellers falder den tilbage på ID fra localStorage
     const unsub = onSnapshot(doc(FIREBASE_DB, "orders", orderId), (doc) => {
-      setCurrentOrder(doc.data());
-    });
+      setCurrentOrder(doc.data())
+      setLoading(false)
+    })
+
   }, []);
 
   const handleSaveOrderButtonClicked = () => {
@@ -52,103 +60,114 @@ const OrderStatus = () => {
         customLink="/bestil-online"
       />
       <PageWrapperContainer>
-        {!currentOrder && (
+        {loading ?
           <>
-            <p className="text-xl font-semibold">Der er ingen ordre med dette ID.</p>
-            <p>
-              Hvis du mener det er en fejl er du velkommen til at ringe til butikken på tlf.{" "}
-              <span className="font-semibold">22 13 35 78</span>
-            </p>
+            <div className="">
+              <PulseLoader color="#000000" />
+              <p>Søger efter ordre...</p>
+            </div>
           </>
-        )}
-        {currentOrder && (
-          <div className="mt-8 mb-10">
-            <div className="flex justify-between items-center">
-              <h1 className="font-bold text-2xl">Ordre #{currentOrder?.orderNo}</h1>
-              <div className="flex items-center gap-2 text-lg cursor-pointer" onClick={handleShareOrder}>
-                <p className="font-semibold">Del ordre</p>
-                <i className="fa-solid fa-share-from-square text-xl"></i>
-              </div>
-            </div>
-            <div className="mt-12">
-              {currentOrder.status !== "userCancelled" &&
-                currentOrder.status !== "shopCancelled" &&
-                currentOrder.status !== "pending" && <StatusBar status={currentOrder?.status} />}
-            </div>
-            <div className="mt-16 mb-16 flex text-center w-full max-w-max m-auto text-xl relative">
-              {currentOrder?.status === "pending" && <p>"Din bestiling er sendt til butikken"</p>}
-              {currentOrder?.status === "recieved" && <p>"Din bestiling er modtaget"</p>}
-              {currentOrder?.status === "accepted" && <p>"Vi har accepteret din bestilling"</p>}
-              {currentOrder?.status === "ready" && <p>"Din bestiling er klar i butikken"</p>}
-              {currentOrder?.status === "picked" && <p>"Du har afhentet din mad, velbekomme"</p>}
-              {currentOrder?.status === "userCancelled" && <p>"Du har annulleret din ordre"</p>}
-              {currentOrder?.status === "shopCancelled" && <p>"Butikken har annulleret din ordre"</p>}
-              <i className="fa-solid fa-comment absolute -right-2 -top-5 text-8xl opacity-10 text-grey"></i>
-            </div>
-            <div>
-              {currentOrder?.commentsFromShop && (
-                <>
-                  {currentOrder?.commentsFromShop
-                    ?.slice(0)
-                    .reverse()
-                    .map((comment, key) => {
-                      return (
-                        <div key={key} className="flex flex-col gap-2 mb-4">
-                          <div className="flex flex-col">
-                            <p className="text-xl text-primary font-semibold">Besked fra butikken:</p>
-                            <div className="p-4 border-dark border-2 rounded-xl w-full md:min-w-[300px] md:max-w-max">
-                              <p className="text-md font-medium italic max-w-readable">"{comment.messageToCustomer}"</p>
-                            </div>
-                          </div>
-                          <p>{timestampConvert(comment.date.seconds, "stampToPreciseDate")}</p>
-                        </div>
-                      );
-                    })}
-                </>
-              )}
-            </div>
-            <div className="mt-10 full-width">
-              <div className="flex justify-between items-center">
-                <h3 className="font-bold">Afhentningstid</h3>
+          :
+          <>
+            {!currentOrder && (
+              <>
+                <p className="text-xl font-semibold">Der er ingen ordre med dette ID.</p>
                 <p>
-                  {currentOrder.pickup.time !== "Hurtigst muligt"
-                    ? timestampConvert(currentOrder.pickup.time.seconds, "stampToHourMinute")
-                    : "Hurtigst muligt"}
+                  Hvis du mener det er en fejl er du velkommen til at ringe til butikken på tlf.{" "}
+                  <span className="font-semibold">22 13 35 78</span>
                 </p>
+              </>
+            )}
+            {currentOrder && (
+              <div className="mt-8 mb-10">
+                <div className="flex justify-between items-center">
+                  <h1 className="font-bold text-2xl">Ordre #{currentOrder?.orderNo}</h1>
+                  <div className="flex items-center gap-2 text-lg cursor-pointer" onClick={handleShareOrder}>
+                    <p className="font-semibold">Del ordre</p>
+                    <i className="fa-solid fa-share-from-square text-xl"></i>
+                  </div>
+                </div>
+                <div className="mt-12">
+                  {currentOrder.status !== "userCancelled" &&
+                    currentOrder.status !== "shopCancelled" &&
+                    currentOrder.status !== "pending" && <StatusBar status={currentOrder?.status} />}
+                </div>
+                <div className="mt-16 mb-16 flex text-center w-full max-w-max m-auto text-xl relative">
+                  {currentOrder?.status === "pending" && <p>"Din bestiling er sendt til butikken"</p>}
+                  {currentOrder?.status === "recieved" && <p>"Din bestiling er modtaget"</p>}
+                  {currentOrder?.status === "accepted" && <p>"Vi har accepteret din bestilling"</p>}
+                  {currentOrder?.status === "ready" && <p>"Din bestiling er klar i butikken"</p>}
+                  {currentOrder?.status === "picked" && <p>"Du har afhentet din mad, velbekomme"</p>}
+                  {currentOrder?.status === "userCancelled" && <p>"Du har annulleret din ordre"</p>}
+                  {currentOrder?.status === "shopCancelled" && <p>"Butikken har annulleret din ordre"</p>}
+                  <i className="fa-solid fa-comment absolute -right-2 -top-5 text-8xl opacity-10 text-grey"></i>
+                </div>
+                <div>
+                  {currentOrder?.commentsFromShop && (
+                    <>
+                      {currentOrder?.commentsFromShop
+                        ?.slice(0)
+                        .reverse()
+                        .map((comment, key) => {
+                          return (
+                            <div key={key} className="flex flex-col gap-2 mb-4">
+                              <div className="flex flex-col">
+                                <p className="text-xl text-primary font-semibold">Besked fra butikken:</p>
+                                <div className="p-4 border-dark border-2 rounded-xl w-full md:min-w-[300px] md:max-w-max">
+                                  <p className="text-md font-medium italic max-w-readable">"{comment.messageToCustomer}"</p>
+                                </div>
+                              </div>
+                              <p>{timestampConvert(comment.date.seconds, "stampToPreciseDate")}</p>
+                            </div>
+                          );
+                        })}
+                    </>
+                  )}
+                </div>
+                <div className="mt-10 full-width">
+                  <div className="flex justify-between items-center">
+                    <h3 className="font-bold">Afhentningstid</h3>
+                    <p>
+                      {currentOrder.pickup.time !== "Hurtigst muligt"
+                        ? timestampConvert(currentOrder.pickup.time.seconds, "stampToHourMinute")
+                        : "Hurtigst muligt"}
+                    </p>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <h3 className="font-bold">Afhentningsdato</h3>
+                    <p>{timestampConvert(currentOrder.pickup.date.seconds, "stampToDate")}</p>
+                  </div>
+                  <p className="max-w-readable mt-4">
+                    Har du spørgsmål, brug for hjælp eller lignende, er du velkommen til at ringe til butikken på tlf.{" "}
+                    <span className="font-semibold">22 13 35 78</span>
+                  </p>
+                </div>
+                <div className="mt-8">
+                  <CancelOrder
+                    placedAt={currentOrder.orderPlacedAt}
+                    orderId={orderId || currentOrderId}
+                    canCancel={currentOrder.canCancel}
+                  />
+                </div>
+                <div className="mt-5">
+                  <CustomButton
+                    iconRight={true}
+                    function={handleSaveOrderButtonClicked}
+                    customWidth="w-full"
+                    title="Gem bestilling"
+                    icon={"fa-solid fa-heart"}
+                  />
+                </div>
+                <div className="mt-10">
+                  <p className="text-xl font-bold">Opdateringsoversigt</p>
+                  <div className="mt-5">
+                    <UpdatesBar orderId={orderId || currentOrderId} />
+                  </div>
+                </div>
               </div>
-              <div className="flex justify-between items-center">
-                <h3 className="font-bold">Afhentningsdato</h3>
-                <p>{timestampConvert(currentOrder.pickup.date.seconds, "stampToDate")}</p>
-              </div>
-              <p className="max-w-readable mt-4">
-                Har du spørgsmål, brug for hjælp eller lignende, er du velkommen til at ringe til butikken på tlf.{" "}
-                <span className="font-semibold">22 13 35 78</span>
-              </p>
-            </div>
-            <div className="mt-8">
-              <CancelOrder
-                placedAt={currentOrder.orderPlacedAt}
-                orderId={orderId || currentOrderId}
-                canCancel={currentOrder.canCancel}
-              />
-            </div>
-            <div className="mt-5">
-              <CustomButton
-                iconRight={true}
-                function={handleSaveOrderButtonClicked}
-                customWidth="w-full"
-                title="Gem bestilling"
-                icon={"fa-solid fa-heart"}
-              />
-            </div>
-            <div className="mt-10">
-              <p className="text-xl font-bold">Opdateringsoversigt</p>
-              <div className="mt-5">
-                <UpdatesBar orderId={orderId || currentOrderId} />
-              </div>
-            </div>
-          </div>
-        )}
+            )}
+          </>
+        }
       </PageWrapperContainer>
     </>
   );
